4e0dc556819b775e19a225bfd688c6996f4b7f10
duc <simontruongduc@gmail.com>
2022-09-29 22:01:48 +0700

    buoi2

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'/public')));
 // http logger
 app.use(morgan('combined'));
 // template engine
-app.engine('handlebars',engine());
-app.set('view engine','handlebars');
+app.engine('hbs',engine({
+    extname:'.hbs'
+}));
+app.set('view engine','hbs');
 app.set('views', path.join(__dirname,'/resources/views'));
 
 app.get('/',(req, res) => res.render('home'))
